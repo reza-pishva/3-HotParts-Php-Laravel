5127406e6cd9d9bff986ef6226bb8aca3e1f387b
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-01-23 10:06:32 +0330

    Add files via upload

---
+++ resources/views/Ansaldo/AnsaldoOutProgComponents/component06.blade.php
@@ -0,0 +1,80 @@
+<div class="modal fade" id="myModal3" style="direction: rtl;margin-top: 70px">
+    <div class="modal-dialog modal-md" style="margin-top: 25px">
+        <div class="modal-content">
+            <!-- Send Header -->
+            <div class="modal-header" style="height: 35px;padding-top: 5px;background-color:#2F4F4F" >
+                <div class="row" style="width: 100%">
+                    <div class="col-6"><p class="modal-title" style="color: white;font-family: Tahoma;font-size: small;display: inline">اصلاح درخواست ارسال قطعات</p></div>
+                    <div class="col-6">
+                        <div class="row" style="width: 100%">
+                            <div class="col-10"></div>
+                            <div class="col-2">
+                                <button type="button" class="close" data-dismiss="modal" style="text-align: center;display: inline;color: white">&times;</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Send form -->
+            <div class="container"  style="margin: auto;background-color:lightgray;height: 260px ">
+                <form method="post" encType="multipart/form-data" id="tps_edit" action="{{route('out.edit')}}">
+                    {{csrf_field()}}
+                    <div class="row">
+                        <div>
+                            <input type="hidden" class="form-control" id="ID_T_EDIT" name="ID_T_EDIT" >
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-3 ">
+                            <select class="form-control isclicked1" name="ID_TG_EDIT" id="ID_TG_EDIT" required style="width: 200px;font-family: Tahoma;font-size: small;display: inline">
+                                <option value="">انتخاب نوع قطعات</option>
+                                @foreach($ghataats as $ghataat)
+                                    <option value="{{$ghataat->ID_TG}}">{{$ghataat->GHATAAT_NAME}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="col-6" style="margin-right: 100px">
+                            <input type="text" maxlength="80" class="form-control" id="LOCATION_NAME_EDIT"  data-toggle="tooltip" data-placement="right"  name="LOCATION_NAME_EDIT"  style="font-family: Tahoma;font-size: small;width: 100%;" required title="دریافت کننده\ارسال کننده" placeholder="دریافت کننده\ارسال کننده">
+                        </div>
+
+                    </div>
+                    <br>
+                    <div class="row mt-0">
+                        <div class="field row" >
+                            <div class="col-5" style="text-align: center"><p style="text-align: left;font-size: xx-small;font-family: Tahoma">تاریخ ارسال</p></div>
+                            <div class="col-7" style="text-align: center"> <input type="text" maxlength="10" class="form-control" id="DATE_SHAMSI_EDIT"  data-toggle="tooltip" data-placement="right"  name="DATE_SHAMSI_EDIT"  style="font-family: Tahoma;font-size: small;width: 100%;" required title="تاریخ شروع تعمیرات"></div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="field row">
+                        <div class="col-2" style="text-align: right"><p style="text-align: left;font-size: xx-small;font-family: Tahoma">تعداد قطعات</p></div>
+                        <div class="col-4" style="text-align: center"> <input type="number" max="100000" min="0" class="form-control" id="GROUP_COUNT_EDIT"  data-toggle="tooltip" data-placement="right"  name="GROUP_COUNT_EDIT" placeholder="تعداد قطعات" style="font-family: Tahoma;font-size: smaller;width: 80%;" required title="تعداد قطعات"></div>
+                        <div class="col-2 ">
+                            <select class="form-control isclicked1" name="OUT_IN_EDIT" id="OUT_IN_EDIT" required style="width: 200px;font-family: Tahoma;font-size: small;display: inline">
+                                <option value="">ورود یا خروج</option>
+                                <option value="1">ورود</option>
+                                <option value="2">خروج</option>
+                            </select>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="field row mt-0">
+                        <div class="col-9" style="text-align: right"><input type="text" maxlength="150" class="form-control" id="DISCRIPTION_EDIT"  data-toggle="tooltip" data-placement="right"  name="DISCRIPTION_EDIT" placeholder="توضیحات" style="direction: rtl;font-family: Tahoma;font-size: smaller;width: 100%" title="توضیحات"></div>
+                        <div class="col-3" style="text-align: right"><button type="submit" class="btn btn-info" id="btnupdate4" style="font-family: Tahoma;font-size: small;text-align: center;width:80%" >ثبت </button></div>
+                    </div>
+
+                </form>
+                <div id="ajax-alert4" class="alert" style="display:none;font-family: Tahoma;font-size: small;text-align: center"></div>
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer bg-info" style="height: 20px">
+
+            </div>
+
+        </div>
+    </div>
+</div>
