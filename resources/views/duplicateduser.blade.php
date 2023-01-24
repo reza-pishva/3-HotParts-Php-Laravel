1e2d3a739942febb8d00a0f6ca0bcdbd12112d6d
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-01-24 11:06:18 +0330

    Delete duplicateduser.blade.php

--- resources/views/duplicateduser.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app_empty')
-@section('content')
-    <div class="container" >
-      <div class="row" style="background-color:rgba(14,53,126,0.5);height: 300px;margin-top: 50px">
-        <h5  style="margin:auto;font-family: Tahoma;color: white;margin-top: 100px;">کلمه عبور برای شما بصورت پیامک ارسال شده</h5>
-      </div>
-    </div>
-@endsection
