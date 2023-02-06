60b3bf56540887a4f476eecbc1ede0288515148a
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-02-06 08:49:04 +0330

    Update Ansaldo_type_ghataat.php

--- app/Ansaldo_type_ghataat.php
+++ app/Ansaldo_type_ghataat.php
@@ -10,6 +10,10 @@ class Ansaldo_type_ghataat extends Model
         'GHATAAT_NAME',
         'TIME_STANDARD',
         'TYPE_CODE',
+        'SET_COUNT',
+        'COUNTB_REJECT',
+        'TIME_REJECT',
+        'ID_USER',
         
     ];
 }
