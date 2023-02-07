c620a1d806dbe0c83cf471dbb65881f62126a52c
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-02-07 14:31:17 +0330

    Update ansaldo_group_name.blade.php

--- resources/views/Ansaldo/ansaldo_group_name.blade.php
+++ resources/views/Ansaldo/ansaldo_group_name.blade.php
@@ -3,7 +3,8 @@
     <script>
         $(document).ready(function() {
             function toFarsiNumber(n) {
-               
+               const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
                 return n
                     .toString()
                     .split('')
