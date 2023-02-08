01efc1d135103b1b112d8b3e11167017445df542
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-02-08 11:10:07 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,19 @@
+﻿<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
 
 //Ansaldo
 Route::get('/bazsaz-form','AnsaldoBazsazController@create');
