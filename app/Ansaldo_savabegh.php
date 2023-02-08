8f872dc96026345f34db5cf54412866897aff337
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-02-08 11:18:00 +0330

    Update Ansaldo_savabegh.php

--- app/Ansaldo_savabegh.php
+++ app/Ansaldo_savabegh.php
@@ -8,6 +8,13 @@ class Ansaldo_savabegh extends Model
 {
     protected $fillable = [
         
-
+        'ID_E',
+        'TIME_WORK',
+        'DAMAGE_PERCENT',
+        'ID_T',
+        'TYPE_INSTAL',
+        'DISCRIPTION',
+        'SAV_TYPE',
+        'ID_SUB',
     ];
 }
