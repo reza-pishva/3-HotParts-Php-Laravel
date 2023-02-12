31c836f948e64b6ef503f9d638df941c380a9e3e
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-02-12 08:14:16 +0330

    Update Ansaldo_savabegh.php

--- app/Ansaldo_savabegh.php
+++ app/Ansaldo_savabegh.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Ansaldo_savabegh extends Model
 {
     protected $fillable = [        
-        'ID_E',
-        'TIME_WORK',
-        'DAMAGE_PERCENT',
-        'ID_T',
-        'TYPE_INSTAL',
-        'DISCRIPTION',
-        'SAV_TYPE',
-        'ID_SUB',
+        
     ];
 }
