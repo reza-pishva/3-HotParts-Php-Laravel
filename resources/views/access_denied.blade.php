dab6e67a4bdeb792eb5a1d97591b7f33df100f4e
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-02-19 11:21:30 +0330

    Update access_denied.blade.php

--- resources/views/access_denied.blade.php
+++ resources/views/access_denied.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container" style="background-color:rgba(14,53,126,0.5)">
 
-        <h3 style="font-family: Tahoma;color: white">شما مجاز به ورود به این صفحه نیستید</h3>
+        <h2 style="font-family: Tahoma;color: white">شما مجاز به ورود به این صفحه نیستید</h2>
 
 
     </div>
