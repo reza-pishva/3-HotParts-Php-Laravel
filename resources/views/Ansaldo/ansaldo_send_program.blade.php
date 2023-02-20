a4bcd0ea8af7366486cf3e45f2fe39fd36085944
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-02-20 08:02:21 +0330

    Update ansaldo_send_program.blade.php

--- resources/views/Ansaldo/ansaldo_send_program.blade.php
+++ resources/views/Ansaldo/ansaldo_send_program.blade.php
@@ -10,7 +10,15 @@
             });
 
             $("#DATE_BEGIN1_EDIT").prop('readonly', true)
-            
+            function toFarsiNumber(n) {
+                const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+                return n
+                    .toString()
+                    .split('')
+                    .map(x => farsiDigits[x])
+                    .join('');
+            }
             bootstrap.Toast.Default.delay = 2000
             $("#DATE_BEGIN1").persianDatepicker({
                 format: 'YYYY/MM/DD'
