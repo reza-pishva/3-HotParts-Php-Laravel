cc95d85f346d74c771869f4e3d83d078d55519f3
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-02-27 10:21:41 +0330

    Update Ansaldo_resv_bazsazi_ghataat.php

--- app/Ansaldo_resv_bazsazi_ghataat.php
+++ app/Ansaldo_resv_bazsazi_ghataat.php
@@ -1,19 +1,1 @@
-<?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Ansaldo_resv_bazsazi_ghataat extends Model
-{
-    protected $fillable = [
-        'ID_T',
-        'COUNT_TAGH',
-        'ID_USER',
-        'COUNT_GH',
-        'RESV',
-        'DATE_SHAMSI',
-        'FILE_NAME',
-    ];
-}
 
