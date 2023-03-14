52624b8da1bd0b7e7d299dd0a05c9c2e6bead12e
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-03-14 08:04:35 +0330

    Update Ansaldo_send_bazsazi_ghataat.php

--- app/Ansaldo_send_bazsazi_ghataat.php
+++ app/Ansaldo_send_bazsazi_ghataat.php
@@ -9,6 +9,19 @@ class Ansaldo_send_bazsazi_ghataat extends Model
     protected $fillable = [
         'ID_TG',
         'ID_BA',
+        'ID_USER',
+        'DATE_BEGIN',
+        'GROUP_COUNT',
+        'COST_LOW',
+        'COST_MEDUME',
+        'COST_HEAVE',
+        'DISCRIPTION',
+        'SHOMAREH_GHARAR',
+        'DATE_BEGIN1',
+        'GHATAAT_NAME',
+        'DATE_BEGIN1',
+        'TAMIRKAR',
+        'EXIT_NO',
 
     ];
 }
