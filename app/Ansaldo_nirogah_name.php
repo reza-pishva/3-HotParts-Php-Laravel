459c8cb8d3bc946f50c8d061e5b0a4b12dfcf44a
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-03-16 21:41:27 +0330

    Update Ansaldo_nirogah_name.php

--- app/Ansaldo_nirogah_name.php
+++ app/Ansaldo_nirogah_name.php
@@ -1,13 +1,1 @@
-<?php
 
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Ansaldo_nirogah_name extends Model
-{
-    protected $fillable = [
-        'NIROGAH_NAME',
-        
-    ];
-}
