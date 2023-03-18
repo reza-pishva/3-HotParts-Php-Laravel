d50a73ed5088c261c4c6d9cc38f39a34d963acf6
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-03-18 12:24:25 +0330

    Update Ansaldo_tamirkaran.php

--- app/Ansaldo_tamirkaran.php
+++ app/Ansaldo_tamirkaran.php
@@ -1,1 +1,12 @@
-<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ansaldo_tamirkaran extends Model
+{
+    protected $fillable = [
+        'TAMIRKAR',
+        'ID_USER',
+    ];
+}
