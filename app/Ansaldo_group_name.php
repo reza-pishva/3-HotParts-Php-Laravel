45d3a06edb529217088048e21e623a23b60b18dd
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-03-20 22:01:23 +0330

    Update Ansaldo_group_name.php

--- app/Ansaldo_group_name.php
+++ app/Ansaldo_group_name.php
@@ -1,15 +1,1 @@
-<?php
 
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Ansaldo_group_name extends Model
-{
-    protected $fillable = [
-        'ID_TG',
-        'ID_USER',
-        'GROUP_CODE',
-        'GROUP_TYPE',
-    ];
-}
