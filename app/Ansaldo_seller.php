19774effd879131d60a6abb422bc0da00ba35c81
Reza Pishva <122688657+reza-pishva@users.noreply.github.com>
2023-04-03 12:38:11 +0330

    Update Ansaldo_seller.php

--- app/Ansaldo_seller.php
+++ app/Ansaldo_seller.php
@@ -1,3 +1,15 @@
-<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ansaldo_seller extends Model
+{
+    protected $fillable = [
+        'SELLER',
+        'ID_USER',
+        
+    ];
+}
 
 
